f27af3e5c793292354652a138bf5165eb9ad77c2
Matias Alvarez <p1210mizu@gmail.com>
2023-05-30 13:51:11 -0300

    0.9 Commit 30/5 *Carpeta J17.SpringBoot_3 borrada de GitHub

--- J17.SpringBoot_3/src/main/java/com/app/J17/SpringBoot_3/Repository/HabilidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.J17.SpringBoot_3.Repository;
-
-import com.app.J17.SpringBoot_3.Model.Habilidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HabilidadRepository extends JpaRepository <Habilidad, Integer> {
-}
